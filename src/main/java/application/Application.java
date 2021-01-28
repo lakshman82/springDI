296167b954880105f388d33d3c9d778351b4a5e9
lakshman <lakshman82@gmail.com>
2021-01-28 11:54:21 +0530

    Update Application.java

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -8,6 +8,7 @@ public class Application {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         FirstBean bean = context.getBean(FirstBean.class);
+
         bean.populateBeans();
         context.close();
 
