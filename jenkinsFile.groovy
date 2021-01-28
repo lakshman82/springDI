a56eae2d877629650e5efa26021ad10964b6b3a8
lakshman <lakshman82@gmail.com>
2021-01-28 12:47:11 +0530

    jenkins file

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -1,10 +1,25 @@
-node {
-    stage 'Checkout'
+pipeline {
+    agent any
 
-    checkout scm
+    environment {
+        // FOO will be available in entire pipeline
+        FOO = "PIPELINE"
+    }
 
-    stage 'Gradle Static Analysis'
-    withSonarQubeEnv {
-        sh "./gradlew clean sonarqube"
+    stages {
+        stage("local") {
+            environment {
+                // BAR will only be available in this stage
+                BAR = "STAGE"
+            }
+            steps {
+                sh 'echo "FOO is $FOO and BAR is $BAR"'
+            }
+        }
+        stage("global") {
+            steps {
+                sh 'echo "FOO is $FOO and BAR is $BAR"'
+            }
+        }
     }
 }
