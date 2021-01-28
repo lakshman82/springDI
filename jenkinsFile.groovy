57c3b160ac45171b2bf5a42501dbe70faee957d9
lakshman <lakshman82@gmail.com>
2021-01-28 19:05:31 +0530

    Update jenkinsFile.groovy

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -1,24 +1,9 @@
-pipeline {
+pipeline{
     agent any
-
-    environment {
-        // FOO will be available in entire pipeline
-        FOO = "PIPELINE"
-    }
-
-    stages {
-        stage("local") {
-            environment {
-                // BAR will only be available in this stage
-                BAR = "STAGE"
-            }
-            steps {
-                sh 'echo "FOO is $FOO and BAR is $BAR"'
-            }
-        }
-        stage("global") {
-            steps {
-                sh 'echo "FOO is $FOO and BAR is $BAR"'
+    stages{
+        stage('Build'){
+            steps{
+                echo 'thi s is lakshman'
             }
         }
     }
