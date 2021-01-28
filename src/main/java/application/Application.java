85ed1f65b0e34fa8014c9d8520e9be7bc17f5038
lakshman <lakshman82@gmail.com>
2021-01-28 11:36:49 +0530

    removed space

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -8,7 +8,6 @@ public class Application {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         FirstBean bean = context.getBean(FirstBean.class);
-
         bean.populateBeans();
         context.close();
 
